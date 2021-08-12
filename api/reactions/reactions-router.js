143d28198c92765fe03b37760f5bf3e8e5c1b4b1
abe-one <77995299+abe-one@users.noreply.github.com>
2021-08-12 15:01:18 -0500

    Build Reactions Proof-Of-Concept Router and Logic files

---
+++ api/reactions/reactions-router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+const Reactions = require('./reactions-logic')
+
+//! NO VALIDATION NO SANITIZATION
+
+router.post('/celebrate',  (req, res, next) => {
+Reactions.celebrateAllPosts(req.body)
+.then((slackResponses)=>{
+res.status(201).json(slackResponses)
+})
+.catch(next)
+})
+
+module.exports = router
