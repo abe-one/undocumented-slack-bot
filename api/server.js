63eadf6819c310c93b4e34ea0760d4d9dd41c99f
abe-one <77995299+abe-one@users.noreply.github.com>
2021-08-12 15:01:52 -0500

    Connect Reactions Router To Server

--- api/server.js
+++ api/server.js
@@ -3,15 +3,20 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const server = express();
+const reactionsRouter  = require('./reactions/reactions-router')
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
+server.use("/reactions", reactionsRouter)
+
 server.use("*", (_req, res) => {
   res.status(200).json({ message: "server up" });
 });
 
+
+// eslint-disable-next-line no-unused-vars
 server.use((err, _req, res, _next) => {
   res.status(err.status || 500).json({
     message: err.message || "server error",
