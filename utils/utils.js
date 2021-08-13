c4a21fb0db101d6709857ff5f73e04bd9a34411b
abe-one <77995299+abe-one@users.noreply.github.com>
2021-08-13 09:09:14 -0500

    Create axiosWithSlackAuth, defaultFormData functions

---
+++ utils/utils.js
@@ -0,0 +1,26 @@
+const { default: axios } = require("axios");
+const FormData = require("form-data");
+const {
+  DEFAULT_CHANNEL,
+  USER_AUTH_TOKEN,
+  USER_COOKIE,
+  BASE_SLACK_URL,
+} = require("./env-fallbacks");
+
+const defaultFormData = () => {
+  const data = new FormData();
+  data.append("channel", DEFAULT_CHANNEL);
+  data.append("token", USER_AUTH_TOKEN);
+  return data;
+};
+
+const axiosWithSlackAuth = (headers) =>
+  axios.create({
+    headers: { ...headers, cookie: USER_COOKIE },
+    baseURL: BASE_SLACK_URL,
+  });
+
+module.exports = {
+  defaultFormData,
+  axiosWithSlackAuth,
+};
