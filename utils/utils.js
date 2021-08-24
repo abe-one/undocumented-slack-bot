b9dce7a8020916faf3e71a76783a1c3213948b3a
abe-one <77995299+abe-one@users.noreply.github.com>
2021-08-24 10:02:54 -0500

    Cleanup utils

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,4 @@
 const { default: axios } = require("axios");
-const cron = require("node-cron");
 const FormData = require("form-data");
 const {
   DEFAULT_CHANNEL,
@@ -21,46 +20,6 @@ const axiosWithSlackAuth = (headers) =>
     baseURL: BASE_SLACK_URL,
   });
 
-const scheduleSlackRequests = async (
-  frequency,
-  cbToSchedule,
-  formSubmissions
-) => {
-  const responseAndCronJob = {};
-  const cancelMsg = "Cron job canceled";
-  const cronfirmationMsg = `Cron job scheduled for every ${frequency} hour(s)`;
-
-  if (frequency === 0) {
-    console.log(cancelMsg);
-    responseAndCronJob.cronfirmation = cancelMsg;
-  } else {
-    try {
-      let newOldestMessage = `${new Date().getTime() / 1000}`;
-
-      responseAndCronJob.cronfirmation = cronfirmationMsg;
-      responseAndCronJob.firstResponse = await cbToSchedule(formSubmissions);
-
-      responseAndCronJob.job = cron.schedule(
-        `1 */${frequency} * * *`,
-        async () => {
-          try {
-            formSubmissions = { ...formSubmissions, oldest: newOldestMessage };
-            newOldestMessage = `${new Date().getTime() / 1000}`;
-            const followingResponse = await cbToSchedule(formSubmissions);
-
-            console.log(new Date(), ":", cronfirmationMsg, followingResponse);
-          } catch (err) {
-            console.log(err);
-          }
-        }
-      );
-    } catch (err) {
-      return err;
-    }
-  }
-  return responseAndCronJob;
-};
-
 const selectRandomArrayElements = (array, limit) => {
   limit = limit || 23;
   const randomizedSet = new Set();
@@ -77,6 +36,5 @@ const selectRandomArrayElements = (array, limit) => {
 module.exports = {
   defaultFormData,
   axiosWithSlackAuth,
-  scheduleSlackRequests,
   selectRandomArrayElements,
 };
